830a1fa03bf1fd92a1895c1ebefa4bca0dcd6238
EthanHorrigan <ethanhorr@gmail.com>
2019-10-15 16:10:57 +0100

    Project Setup & Initial Commit

---
+++ grpc-async-password/src/main/java/ie/gmit/ds/grpc_async_password/App.java
@@ -0,0 +1,13 @@
+package ie.gmit.ds.grpc_async_password;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
